ca3ec7d9e70b01e3e80e7714d71db4cd8e85383c
Takeya Suzuki <daikya@gmail.com>
2020-06-06 17:42:14 +0900

    lesson04

--- mamazon/models.py
+++ mamazon/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
-class Products(models.Model):
+class Product(models.Model):
     name = models.CharField(max_length=150)
     price = models.IntegerField()
     description = models.TextField()
     thumbnail = models.ImageField(upload_to='thumbnails/')
+
+    def __str__(self):
+        return self.name
